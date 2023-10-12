084287261ad6154152f9d4673899e6bc85f822b7
A-freedom <fondcanon@12>
2023-10-12 22:01:39 +0300

    add docker support

--- ytdl_source.py
+++ ytdl_source.py
@@ -1,13 +1,13 @@
 import discord
 import yt_dlp
 import asyncio
-import os
+from pathlib import Path
 
 # Define the output folder for downloaded files
-output_folder = ".\data\youtube"
+output_folder = Path("data/youtube")
 
 # Create the output folder if it doesn't exist
-os.makedirs(output_folder, exist_ok=True)
+output_folder.mkdir(parents=True, exist_ok=True)
 
 ytdl_format_options = {
     'format': 'bestaudio/best',
@@ -19,12 +19,11 @@ ytdl_format_options = {
     'quiet': True,
     'no_warnings': True,
     'default_search': 'auto',
-    'outtmpl': f'{output_folder}\%(title)s.%(ext)s',  # Use video title as the filename
+    'outtmpl': str(output_folder / "%(id)s.%(ext)s"),  # Use the video ID as the filename
 }
 
 # Initialize yt_dlp instance
 ytdl = yt_dlp.YoutubeDL(ytdl_format_options)
-
 
 class YTDLSource(discord.PCMVolumeTransformer):
     def __init__(self, source, *, data, volume=0.5):
@@ -39,9 +38,6 @@ class YTDLSource(discord.PCMVolumeTransformer):
         loop = loop or asyncio.get_event_loop()
         data = await loop.run_in_executor(None, lambda: ytdl.extract_info(url, download=not stream))
         if 'entries' in data:
-            # take the first item from a playlist
             data = data['entries'][0]
-        filename = data['title'] if stream else ytdl.prepare_filename(data)
-        full_file_path = filename
-        print(full_file_path)
-        return [full_file_path,data['title']]
+        filename = data['id'] if stream else ytdl.prepare_filename(data)
+        return [filename, data['title']]
